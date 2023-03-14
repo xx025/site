531ea1327db83f80e9ed1f3ed5d054d83b47a5d2
xx025 <71559822+xx025@users.noreply.github.com>
2023-03-14 11:39:42 +0800

    init

--- static/js/global.js
+++ static/js/global.js
@@ -1,23 +1,27 @@
+window.onload = function () {
+
 // 让所有url 变成可点击的a 标签
 // 获取所有的文本节点
-const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    const textNodes = document.evaluate("//text()", document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 // 遍历所有的文本节点
-for (let i = 0; i < textNodes.snapshotLength; i++) {
-    const node = textNodes.snapshotItem(i);
-    // 判断文本节点是否包含 URL
-    if (node.parentElement.tagName.toLowerCase() !== "a" && node.nodeValue.match(/\bhttps?:\/\/\S+/gi)) {
-        const url = node.nodeValue.match(/\bhttps?:\/\/\S+/gi)[0];
-        const link = document.createElement("a");
-        link.href = url;
-        link.textContent =node.data;
-        node.parentNode.replaceChild(link, node);
+    for (let i = 0; i < textNodes.snapshotLength; i++) {
+        const node = textNodes.snapshotItem(i);
+        // 判断文本节点是否包含 URL
+        if (node.parentElement.tagName.toLowerCase() !== "a" && node.nodeValue.match(/\bhttps?:\/\/\S+/gi)) {
+            const url = node.nodeValue.match(/\bhttps?:\/\/\S+/gi)[0];
+            const link = document.createElement("a");
+            link.href = url;
+            link.textContent = node.data;
+            node.parentNode.replaceChild(link, node);
+        }
     }
+
+
+    const h1 = document.querySelector('#free-chatgpt-site-list');
+    const a = document.createElement('a');
+    a.textContent = h1.textContent;
+    a.href = '/';
+    h1.innerHTML = '';
+    h1.appendChild(a);
+
 }
-
-
-const h1 = document.querySelector('#free-chatgpt-site-list');
-const a = document.createElement('a');
-a.textContent = h1.textContent;
-a.href = '/';
-h1.innerHTML = '';
-h1.appendChild(a);
--- static/js/zs.js
+++ static/js/zs.js
@@ -1,41 +1,45 @@
-var adContainer = document.createElement('div');
-adContainer.style.position = 'fixed';
-adContainer.style.bottom = '0';
-adContainer.style.right = '0'
-adContainer.style.width = '20%';
-adContainer.style.backgroundColor = '#f1f1f1';
-adContainer.style.borderTop = '1px solid #ccc';
-document.body.appendChild(adContainer);
+window.onload = function () {
+    var adContainer = document.createElement('div');
+    adContainer.style.position = 'fixed';
+    adContainer.style.bottom = '0';
+    adContainer.style.right = '0'
+    adContainer.style.width = '20%';
+    adContainer.style.backgroundColor = '#f1f1f1';
+    adContainer.style.borderTop = '1px solid #ccc';
+    document.body.appendChild(adContainer);
 
 
-let zsurl = '/static/imgs/wxds.png'
-var adLink = document.createElement('a');
-adLink.href = zsurl;
-adLink.target = '_blank';
-adContainer.appendChild(adLink);
+    let zsurl = '/static/imgs/wxds.png'
+    var adLink = document.createElement('a');
+    adLink.href = zsurl;
+    adLink.target = '_blank';
+    adContainer.appendChild(adLink);
 
-var adImage = document.createElement('img');
-adImage.src = zsurl;
-adImage.alt = 'Advertisement';
-adImage.style.width = '100%';
-adLink.appendChild(adImage);
+    var adImage = document.createElement('img');
+    adImage.src = zsurl;
+    adImage.alt = 'Advertisement';
+    adImage.style.width = '100%';
+    adLink.appendChild(adImage);
 
-var collapseButton = document.createElement('button');
-collapseButton.innerText = '-';
-collapseButton.style.position = 'absolute';
-collapseButton.style.top = '0';
-collapseButton.style.left = '0';
-adContainer.appendChild(collapseButton);
+    var collapseButton = document.createElement('button');
+    collapseButton.innerText = '-';
+    collapseButton.style.position = 'absolute';
+    collapseButton.style.top = '0';
+    collapseButton.style.left = '0';
+    adContainer.appendChild(collapseButton);
 
-var isCollapsed = false;
+    var isCollapsed = false;
 
-collapseButton.addEventListener('click', function () {
-    isCollapsed = !isCollapsed;
-    if (isCollapsed) {
-        adContainer.style.height = '40px'; // 折叠起来，高度为 40px
-        collapseButton.innerText = '+';
-    } else {
-        adContainer.style.height = 'auto'; // 展开，高度为页面高度的 20%
-        collapseButton.innerText = '-';
-    }
-});
+    collapseButton.addEventListener('click', function () {
+        isCollapsed = !isCollapsed;
+        if (isCollapsed) {
+            adContainer.style.height = '40px'; // 折叠起来，高度为 40px
+            collapseButton.innerText = '+';
+        } else {
+            adContainer.style.height = 'auto'; // 展开，高度为页面高度的 20%
+            collapseButton.innerText = '-';
+        }
+    });
+
+}
+
